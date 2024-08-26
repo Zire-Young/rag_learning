7609c74d7b33882e7da2f0509183d5175815f4dd
Zire Young <91858772+Zire-Young@users.noreply.github.com>
2024-08-26 20:24:42 +0800

    Add files via upload

---
+++ nltk_download.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+mkdir ./nltk_data
+cd nltk_data
+git clone https://gitee.com/yzy0612/nltk_data.git  --branch gh-pages
+cd nltk_data
+mv packages/*  ./
+cd tokenizers
+unzip punkt.zip
+cd ../taggers
+unzip averaged_perceptron_tagger.zip
